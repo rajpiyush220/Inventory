a1ee8af011df595ac91103b4bdd911b5a26a9c9d
Mukesh <kmuke@indosakura.com>
2022-12-11 23:49:05 +0900

    test

---
+++ Inventory/src/main/resources/db/migration/V2_2022120111428300575__abc.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `user_test` (
+  `id` binary(16) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created` datetime(6) NOT NULL,
+  `updated` datetime(6) NOT NULL,
+  `version` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_AK_username` (`username`)
+);
