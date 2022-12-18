1a44361040496638867fbd1d02f90c4412a96399
Piyush <piyush@7classes.com>
2022-12-18 21:41:36 +0530

    Removed unnecessary code

--- Inventory/src/main/resources/db/migration/V2_2022120121336340294__Alter_Stock_Audit_Table.sql
+++
@@ -1,3 +0,0 @@
-CALL `AddColumnIfNotExistsAfterColumn`('99mall_inventory', 'stock_audit', 'stock_id', 'binary(16) not null', 'product_id');
-CALL `AddForeignKeyIfNotExists`('99mall_inventory', 'stock_audit', 'stock_id', 'stock', 'id');
-
