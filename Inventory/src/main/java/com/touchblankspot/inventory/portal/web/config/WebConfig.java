2b90d4b247680412a1c112142857998154822a41
Touch Blank Spot <touchblankspot@gmail.com>
2022-12-28 11:31:18 +0530

    Copyright added from props

--- Inventory/src/main/java/com/touchblankspot/inventory/portal/web/config/WebConfig.java
+++ Inventory/src/main/java/com/touchblankspot/inventory/portal/web/config/WebConfig.java
@@ -1,25 +1,15 @@
 package com.touchblankspot.inventory.portal.web.config;
 
-import java.util.Locale;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Locale;
+
 @Configuration
 public class WebConfig {
-
-  @Bean
-  public MessageSource messageSource() {
-    ReloadableResourceBundleMessageSource messageSource =
-        new ReloadableResourceBundleMessageSource();
-    messageSource.setBasename("classpath:validation");
-    messageSource.setDefaultEncoding("UTF-8");
-    return messageSource;
-  }
 
   @Bean
   public LocaleResolver localeResolver() {
