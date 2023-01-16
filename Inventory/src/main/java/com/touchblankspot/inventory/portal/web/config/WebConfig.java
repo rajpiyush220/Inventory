64d66657ff3640b2254220499ef921af3f344e77
touchblankspot <113407164+touchblankspot@users.noreply.github.com>
2023-01-16 18:17:40 +0530

    Feature/price updare (#40)
    * Product price updatesd
    
    * External/secret config added
    
    Co-authored-by: Piyush <rajpiyush220@gmail.com>

--- Inventory/src/main/java/com/touchblankspot/inventory/portal/web/config/WebConfig.java
+++ Inventory/src/main/java/com/touchblankspot/inventory/portal/web/config/WebConfig.java
@@ -1,14 +1,30 @@
 package com.touchblankspot.inventory.portal.web.config;
 
 import java.util.Locale;
+import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig {
+
+  @Value("${application.smtp.host}")
+  private String smtpHost;
+
+  @Value("${application.smtp.username}")
+  private String smtpUser;
+
+  @Value("${application.smtp.secret}")
+  private String smtpSecret;
+
+  @Value("${application.smtp.port}")
+  private Integer smtpPort;
 
   @Bean
   public LocaleResolver localeResolver() {
@@ -22,5 +38,19 @@ public class WebConfig {
     LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
     lci.setParamName("lang");
     return lci;
+  }
+
+  @Bean
+  public JavaMailSender getJavaMailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost(smtpHost);
+    mailSender.setPort(smtpPort);
+    mailSender.setUsername(smtpUser);
+    mailSender.setPassword(smtpSecret);
+    Properties props = mailSender.getJavaMailProperties();
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    return mailSender;
   }
 }
