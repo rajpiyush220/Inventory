f787b193d6d7dd8ab4dcb71d7cafd87091bf28a2
Piyush <rajpiyush220@gmail.com>
2023-02-01 09:38:26 +0530

    Image pus pull added

--- Inventory/deploy.sh
+++ Inventory/deploy.sh
@@ -1,2 +1,3 @@
-git pull
-sudo systemctl restart 99mall.service
+cat cred.txt | docker login --username devtouchblankspot --password-stdin
+docker build -t devtouchblankspot/99mall_inventory:latest .
+docker push devtouchblankspot/99mall_inventory:latest
